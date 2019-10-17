3210bc3ffc6279a11cbe29211a3573f0d4d65e32
Salem Asrat <salemasrat@gmail.com>
2019-10-17 18:06:08 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+require "pry"
+def square_array(numbers)
+   new_numbers = []
+  numbers.each do |number|
+     new_numbers << number ** 2
+   
+  end
+  new_numbers
 end
